0b3eccb2c7481d21fa31025f3398843fbb345b9c
Naga Sandeep Ramachandruni <sandeep@heallo.ai>
2019-02-12 18:50:55 +0530

    added support for oversampling

--- random_copy.py
+++ random_copy.py
@@ -1,8 +1,11 @@
 from random import shuffle
 import sys, shutil, os
-
+import random
 from os import listdir
 from os.path import isfile, join
+import string
+def random_str(num):
+    return ''.join(random.choice(string.ascii_uppercase + string.digits) for _ in range(num))
 
 mypath = sys.argv[1]
 newpath = sys.argv[2]
@@ -14,7 +17,19 @@ shuffle(onlyfiles)
 
 number = int(sys.argv[3])
 
-copyfiles = onlyfiles[:number]
+if number < len(onlyfiles):
+    copyfiles = onlyfiles[:number]
+else:
+    copyfiles = onlyfiles*(int(number/len(onlyfiles))+1)
+    print(len(copyfiles))
+    copyfiles = copyfiles[:number]
+
 
 for f in copyfiles:
-    shutil.copy(f, newpath)
+    dest = os.path.join(newpath, os.path.basename(f))
+    if os.path.exists(dest):
+       newname = os.path.join(newpath, random_str(3)+os.path.basename(f))
+       print(newname)
+       shutil.copy(f, newname)
+    else:
+       shutil.copy(f, dest)

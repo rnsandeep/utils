064361d835288d962b3b983186d81ef3ab7a1744
Naga Sandeep Ramachandruni <sandeep@heallo.ai>
2019-02-25 18:09:01 +0530

    fixed bugs

--- random_copy.py
+++ random_copy.py
@@ -1,7 +1,7 @@
 import sys, shutil, os
 import random
 import string
-from file_utils import getfiles, create_dir
+from file_utils import files_with_ext, create_dir
 
 def random_str(N):
     # generate random string of length N
@@ -10,18 +10,20 @@ def random_str(N):
 mypath = sys.argv[1]
 newpath = sys.argv[2]
 
-create_dir(mypath)
+create_dir(newpath)
+
 jpegfiles = files_with_ext(mypath, '.jpg') # get all the files with extentsion
 
 # fo random shuffle of files
-random.shuffle(files)
+random.shuffle(jpegfiles)
 
 number = int(sys.argv[3])
 
-if number < len(onlyfiles):
-    copyfiles = onlyfiles[:number]
+if number < len(jpegfiles):
+    copyfiles = jpegfiles[:number]
 else:
-    copyfiles = onlyfiles*(int(number/len(onlyfiles))+1)
+    copyfiles = jpegfiles*(int(number/len(jpegfiles))+1)
+
     print(len(copyfiles))
     copyfiles = copyfiles[:number]
 
